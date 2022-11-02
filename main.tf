8b2d9275492f569d9eb230f1a86ad1c3044da1d8
ismahan-abidi <ismahen.abidi1992@gmail.com>
2022-11-02 22:32:52 +0100

    provision ec2 instances with count

--- main.tf
+++ main.tf
@@ -1,26 +1,15 @@
 provider "aws" {
 region = "us-east-1"
-access_key = "AKIASBMMRQK27A3CPY6B"
-secret_key = "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"
+access_key = "--------------------------"
+secret_key = "----------------------------"
 
 }
-
-variable "instances" {
-type = map
-default = {
-  clef1 = "ami-08c40ec9ead489470"
-  clef2 = "ami-06640050dc3f556bb"
-
-
- }
-}
-
-resource "aws_instance" "web" {
-for_each = var.instances
-ami = each.value
-instance_type = "t2.micro"
-tags = {
-Name = each.key
-
-}
+resource "aws_instance" "web-inst" {
+  count = 2 # generate two similar EC2 instances
+  ami           = "ami-0ed9277fb7eb570c9"
+  instance_type = "t3.xlarge"
+  tags = {
+    Name = "Instance ${count.index}"
+    Owner = "Bits Lovers"
+  }
 }
